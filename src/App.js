245c55ba0cd8c02819a66b6ab567bdae74a1a5e1
Mochamad Alghifari <mo.alghifari@gmail.com>
2020-05-29 16:44:56 +0700

    Add news video form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect} from 'react';
+import {
+  Card,
+  Form,
+  FormGroup,
+  Button,
+  Col,
+  Label,
+  CustomInput
+} from 'reactstrap';
 
-function App() {
+const App = () => {
+  const [newsVideo, setNewsVideo] = useState(undefined);
+  const [newsVideoError, setNewsVideoError] = useState(undefined);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  const errorSetters = {
+    newsVideo: setNewsVideoError,
+  }
+  const onSubmit = async () => {
+    try {
+      setLoading(true);
+      if (newsVideo) {
+        // const newsVideoResponse = await postEmployeeFile({
+          // employee: employeeId || employeeResponse._id,
+          // file: newsVideo,
+          // type: 'indonesianId'
+        // });
+
+        // if (newsVideoResponse.error) {
+        //   setLoading(false);
+        //   const { code } = newsVideoResponse.error;
+        //   switch (code) {
+        //     case 500:
+        //       throw new Error('Could not contact the server');
+        //     case 400:
+        //       throw new Error('Bad Request');
+        //     case 401:
+        //       throw new Error('You are unauthorized');
+        //     case 403:
+        //       throw new Error('Forbidden');
+        //     default:
+        //       throw new Error('Missing error code');
+        //   }
+        // }
+
+        // newsVideoId = newsVideoResponse._id;
+      }
+    } catch (e) {
+      setLoading(false);
+      setError(e.message);
+    }
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="video-card">
+        <Card body>
+          <div className="video-form">
+            <Form>
+              <FormGroup row>
+                <Col sm={12}>
+                  <CustomInput
+                    type="file"
+                    accept="video/*"
+                    id="newsVideo"
+                    name="customFile"
+                    label="Masukan video berita"
+                  />
+                </Col>
+              </FormGroup>
+              <Button
+                className="btn-purple pill btn btn-secondary font-weight-bold"
+                onClick={onSubmit}
+                >
+                Submit
+              </Button>
+            </Form>
+          </div>
+        </Card>
+      </div>
     </div>
   );
 }
